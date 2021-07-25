b0ce18920e5c834f9c031dadaad05e3004f7e764
Pulkit Bajaj <pulkit171112236@gmail.com>
2021-07-25 14:06:51 +0530

    renamed MusicWire, migrations

--- musicwire/wsgi.py
+++ musicwire/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Musicwire project.
+WSGI config for musicwire project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Musicwire.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicwire.settings')
 
 application = get_wsgi_application()
