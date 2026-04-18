dad2882c19ce8ae6665ec332835deaa655156b5d
pulkit <pulkit171112236@gmail.com>
2026-04-18 16:45:55 +0530

    frontend | added BASE_PATH from environment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
   return {
     server: {
       host: '0.0.0.0',
       port: 3000
     },
-    base: process.env.VITE_BASE_URL || '/',
     plugins: [react()],
+    base: env.BASE_PATH || '/',
   }
 })
